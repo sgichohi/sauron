6eec757ec2c26628d2877ac0931118d0245b4e7d
jfrankle <jfrankle@princeton.edu>
2014-01-13 18:05:18 -0500

    Camera directory specified as command line flag

--- camera2/src/camera.cpp
+++ camera2/src/camera.cpp
@@ -202,6 +202,7 @@ int main(int argc, char** argv) {
   if (argc < 3) {
     cerr << "Usage: camera [Unique ID] [Server Port]\n";
     cerr << "Options: [-v OR --verbose] [-r OR --reconnect]\n";
+    cerr << "[-d OR --directory {dir to store output}] [-m or --max {max number of sendables}]\n";
     return 1;
   }
 
@@ -218,6 +219,7 @@ int main(int argc, char** argv) {
     if (str == "-v" || str == "--verbose") verbose = true; 
     if (str == "-r" || str == "--reconnect") reconnect = true;
     if (str == "-m" || str == "--max") max = stoi(argv[++i]);
+    if (str == "-d" || str == "--directory") dir = string(argv[++i]);
   }  
   
   // Create directories as necessary
